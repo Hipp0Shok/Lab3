c6543964a908c162ff5506865f8a5b55568baccc
Hipp0Shok <gleb.zhivotov@mail.ru>
2018-12-04 23:16:58 +0300

    LAboratory 3

---
+++ hashtable.h
@@ -0,0 +1,476 @@
+#ifndef HASHTABLE_H
+#define HASHTABLE_H
+#include "fstream"
+#include "cmath"
+#include <iostream>
+
+template<typename T1, typename T2>
+class HashTable;
+
+template<typename T1, typename T2>
+std::ofstream& operator << (std::ofstream& stream, HashTable<T1, T2> const & existing)
+{
+    stream << existing.length << std::endl;
+    typename HashTable<T1, T2>::Node* node;
+    for(unsigned int i = 0; i < existing.tableSize; i++)
+    {
+        if(existing.hashTable[i])
+        {
+            node = existing.hashTable[i];
+            while(node)
+            {
+                stream << *(node->key) << std::endl;
+                stream << *(node->object) << std::endl;
+                node = node->next;
+            }
+        }
+    }
+}
+
+template<typename T1, typename T2>
+std::ifstream& operator >> (std::ifstream& stream, HashTable<T1, T2> &existing)
+{
+    unsigned int length;
+    existing.deleteAll();
+    stream >> length;
+    T1 key;
+    T2 value;
+    for(unsigned int i =0; i < length; i++)
+    {
+        stream >> key;
+        stream >> value;
+        existing.addPair(key, value);
+    }
+}
+
+template<typename T1, typename T2>
+class HashTable
+{
+private:
+    struct Node;
+public:
+    class Iterator
+    {
+    public:
+      Iterator();
+      ~Iterator();
+      friend HashTable;
+      Iterator(Iterator const &);
+      Iterator(Node*, const HashTable *parent);
+      Iterator& operator ++();
+      Iterator operator ++(int);
+      bool operator == (Iterator const &) const;
+      bool operator != (Iterator const &) const;
+      T1 operator *();
+      T2 getValue() const;
+   private:
+      Node *node;
+      const HashTable *parent;
+    };
+    friend Iterator;
+    HashTable();
+    ~HashTable();
+    HashTable(HashTable const &);
+    void addPair(T1 const &, T2 const &);
+    bool isPresent(T1 const) const;
+    void deleteKey(T1 const);
+    void deleteAll();
+    unsigned int getLength() const {return length;}
+    Iterator begin();
+    Iterator end();
+    bool operator == (HashTable const &) const;
+    bool operator != (HashTable const &) const;
+    T2& operator [] (T1 const &) const;
+    static unsigned int hash(T1 const &key);
+    friend std::ofstream& operator << <T1, T2> (std::ofstream&, HashTable const &);
+    friend std::ifstream& operator >> <T1, T2> (std::ifstream& stream, HashTable &existing);
+private:
+    struct Node
+    {
+        T2* object;
+        T1* key;
+        Node* next = nullptr;
+        ~Node();
+    };
+    unsigned int length;
+    unsigned int tableSize;
+    Node** hashTable;
+
+};
+
+template<typename T1, typename T2>
+unsigned int HashTable<T1, T2>::hash(T1 const &key)
+{
+    return(static_cast<unsigned int>(1024*fmod(key*0.618033, 1)));
+}
+
+template<typename T1, typename T2>
+HashTable<T1, T2>::HashTable():
+    length(0),
+    tableSize(256),
+    hashTable(new Node*[256])
+{
+    for(unsigned int i = 0; i <= tableSize; i++)
+    {
+        hashTable[i] = nullptr;
+    }
+}
+
+template<typename T1, typename T2>
+HashTable<T1, T2>::Node::~Node()
+{
+    delete object;
+    delete [] key;
+    if(next)
+    {
+        delete next;
+    }
+}
+
+template<typename T1, typename T2>
+HashTable<T1, T2>::~HashTable()
+{
+    for(unsigned int i = 0; i < tableSize; i++)
+    {
+        if(hashTable[i])
+        {
+        delete hashTable[i];
+        hashTable[i] = nullptr;
+        }
+    }
+}
+// Пользователь даёт ключ и значение
+// хэш не зависит от значение, а зависит от ключа
+// полученный ключ отправляется во внешнюю функцию хэша
+// в классе берётся остаток от деления хэша на размер таблицы, чтобы получить индекс
+// запись хранит ещё и ключ для проверок соответствия
+// хэш != ключ
+// шаблон лучше создать сразу
+
+template<typename T1, typename T2>
+void HashTable<T1, T2>::addPair(T1 const &key, T2 const &value)
+{
+    unsigned int bucket;
+    bucket = hash(key) % tableSize;
+    Node* existingNode;
+    if(hashTable[bucket])
+    {
+        existingNode = hashTable[bucket];
+    }
+    else
+    {
+        existingNode = nullptr;
+    }
+    if(isPresent(key))
+    {
+
+       while(existingNode)
+       {
+           if(*(existingNode->key) == key)
+           {
+               *(existingNode->object) = value;
+               break;
+           }
+       }
+    }
+    else
+    {
+        Node* newNode = new Node;
+        if(hashTable[bucket])
+        {
+            newNode->next = existingNode;
+        }
+        hashTable[bucket] = newNode;
+        newNode->object = new T2(value);
+        newNode->key = new T1(key);
+        length++;
+    }
+}
+
+template<typename T1, typename T2>
+bool HashTable<T1, T2>::isPresent(T1 const key) const
+{
+    unsigned int bucket = hash(key) % tableSize;
+    if(hashTable[bucket])
+    {
+        Node* existingNode = hashTable[bucket];
+        while (existingNode)
+        {
+            if(*(existingNode->key) == key)
+            {
+                return true;
+            }
+            existingNode = existingNode->next;
+        }
+    }
+    return false;
+}
+
+template<typename T1, typename T2>
+void HashTable<T1, T2>::deleteKey(T1 const key)
+{
+
+    if(isPresent(key))
+    {
+        unsigned int bucket;
+        bucket = hash(key) % tableSize;
+        Node* current = hashTable[bucket];
+        Node* previous = nullptr;
+        if(!(current->next))
+        {
+                delete current;
+                hashTable[bucket] = nullptr;
+        }
+        else
+        {
+            if(*(current->key) == key)
+            {
+                if(!previous)
+                {
+                    hashTable[bucket] = current->next;
+                    delete current;
+                }
+            }
+            else
+            {
+                previous = current;
+                current = current->next;
+                while(current)
+                {
+                    if(*(current->key) == key)
+                    {
+                        previous->next = current->next;
+                        delete current;
+                        break;
+                    }
+                    current = current->next;
+                }
+            }
+        }
+        length--;
+    }
+}
+
+template<typename T1, typename T2>
+void HashTable<T1, T2>::deleteAll()
+{
+    for(unsigned int i = 0; i < tableSize; i++)
+    {
+        delete hashTable[i];
+        hashTable[i] = nullptr;
+    }
+    length = 0;
+}
+
+template<typename T1, typename T2>
+T2& HashTable<T1, T2>::operator[](T1 const &key) const
+{
+    if(isPresent(key))
+    {
+        Node* current = hashTable[hash(key)%tableSize];
+        while(current)
+        {
+            if(*(current->key) == key)
+            {
+                return *(current->object);
+            }
+            current = current->next;
+        }
+    }
+}
+
+template<typename T1, typename T2>
+bool HashTable<T1, T2>::operator==(HashTable const &existing) const
+{
+    unsigned int i;
+    int key;
+    if(length != existing.length)
+    {
+        return false;
+    }
+    for( i = 0; i < tableSize; i ++)
+    {
+        if(hashTable[i])
+        {
+            key = *(hashTable[i]->key);
+            if(existing.isPresent(key))
+            {
+                if(this->operator[](key) != existing[key])
+                {
+                        return false;
+                }
+            }
+        }
+    }
+    return true;
+}
+
+
+template<typename T1, typename T2>
+bool HashTable<T1, T2>::operator != (HashTable const &existing) const
+{
+    return(!(*this == existing));
+}
+
+template<typename T1, typename T2>
+HashTable<T1, T2>::HashTable(HashTable const &existing):
+    length(0),
+    tableSize(existing.tableSize),
+    hashTable(new Node*[tableSize])
+{
+    for(unsigned int i = 0; i <= tableSize; i++)
+    {
+        hashTable[i] = nullptr;
+    }
+    unsigned int i;
+    for( i = 0 ; i < tableSize ; i++ )
+    {
+        if(existing.hashTable[i])
+        {
+            addPair(*(existing.hashTable[i]->key), *(existing.hashTable[i]->object));
+        }
+    }
+}
+
+template<typename T1, typename T2>
+HashTable<T1, T2>::Iterator::Iterator():
+    node(nullptr),
+    parent(nullptr)
+{
+
+}
+
+template<typename T1, typename T2>
+HashTable<T1, T2>::Iterator::~Iterator()
+{
+
+}
+
+template<typename T1, typename T2>
+HashTable<T1, T2>::Iterator::Iterator(HashTable::Iterator const &existing):
+    node(existing.node),
+    parent(existing.parent)
+{
+}
+
+template<typename T1, typename T2>
+typename HashTable<T1, T2>::Iterator& HashTable<T1, T2>::Iterator::operator ++()
+{
+    unsigned int i;
+    for( i = 0; i < parent->tableSize; i++)
+    {
+        if(parent->hashTable[i] == node)
+        {
+            break;
+        }
+    }
+    i++;
+    while(parent->hashTable[i] && i < parent->tableSize)
+    {
+        i++;
+    }
+    if(i != parent->tableSize)
+    {
+        node = parent->hashTable[i];
+    }
+    return *this;
+}
+
+template<typename T1, typename T2>
+typename HashTable<T1, T2>::Iterator HashTable<T1, T2>::Iterator::operator++(int)
+{
+    unsigned int i;
+    HashTable<T1, T2>::Iterator prev = *this;
+    for( i = 0; i < parent->tableSize; i++)
+    {
+        if(parent->hashTable[i] == node)
+        {
+            break;
+        }
+    }
+    i++;
+    while(!parent->hashTable[i] && i < parent->tableSize)
+    {
+        i++;
+    }
+    if(i != parent->tableSize)
+    {
+        node = parent->hashTable[i];
+    }
+    return  prev;
+}
+
+template<typename T1, typename T2>
+T1 HashTable<T1, T2>::Iterator::operator*()
+{
+    return (*(node->key));
+}
+
+template<typename T1, typename T2>
+T2 HashTable<T1, T2>::Iterator::getValue() const
+{
+    return (*(node->object));
+}
+
+template<typename T1, typename T2>
+bool HashTable<T1, T2>::Iterator::operator==(Iterator const &existing) const
+{
+    if(*(node->object) == *(existing.node->object) &&
+            *(node->key) == *(existing.node->key) &&
+            parent == existing.parent)
+    {
+        return true;
+    }
+    return false;
+}
+
+template<typename T1, typename T2>
+bool HashTable<T1, T2>::Iterator::operator!=(Iterator const &existing) const
+{
+    return(!(this->operator==(existing)));
+}
+
+template<typename T1, typename T2>
+typename HashTable<T1, T2>::Iterator HashTable<T1, T2>::begin()
+{
+    HashTable<T1, T2>::Iterator *iter;
+    if(length > 0)
+    {
+        unsigned int i = 0 ;
+        while (!hashTable[i])
+        {
+            i++;
+        }
+        iter = new HashTable<T1, T2>::Iterator(hashTable[i], this);
+
+    }
+    return *iter;
+}
+
+template<typename T1, typename T2>
+typename HashTable<T1, T2>::Iterator HashTable<T1, T2>::end()
+{
+    HashTable<T1, T2>::Iterator *iter;
+    if(length > 0)
+    {
+        unsigned int i = tableSize;
+        while (!hashTable[i])
+        {
+            i--;
+        }
+        iter = new HashTable<T1, T2>::Iterator(hashTable[i], this);
+
+    }
+    return *iter;
+}
+
+template<typename T1, typename T2>
+HashTable<T1, T2>::Iterator::Iterator(typename HashTable<T1, T2>::Node* existing,
+                                      const HashTable<T1, T2> *parentNew)
+{
+    node = existing;
+    parent = parentNew;
+}
+
+
+#endif // HASHTABLE_H
---
+++ main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include "hashtable.h"
+#include "cassert"
+
+using namespace std;
+template<typename T1, typename T2>
+void output(HashTable<T1, T2> &existing)
+{
+    typename HashTable<T1, T2>::Iterator iter;
+    if(existing.getLength() != 0)
+    {
+        iter = existing.begin();
+        do
+        {
+            std::cout << "Key: " << *iter << " Value: " << iter.getValue() << endl;
+        }while(iter++ != existing.end());
+    }
+}
+
+int main()
+{
+    HashTable<int, int> table;
+    assert(table.getLength() == 0);
+    std::cout << "1" << std::endl;
+    table.addPair(1, 1);
+    std::cout << "2" << std::endl;
+    assert(table.getLength() == 1);
+    table.addPair(1, 2);
+        std::cout << "3" << std::endl;
+    assert((table.getLength() == 1));
+    table.addPair(3, 5);
+    std::cout << "4" << std::endl;
+    assert(table.isPresent(1));
+    assert(!table.isPresent(2));
+
+    table.deleteKey(1);
+
+    assert(!table.isPresent(1));
+    assert(table[3] == 5);
+    HashTable<int, int> table2(table);
+    assert(table == table2);
+    table2.addPair(26125, 15);
+    assert(table2 != table);
+    std::ofstream file;
+    file.open("Data.txt", std::ios::out);
+    file << table2;
+    HashTable<int, int> table3;
+    file.close();
+    std::ifstream file2;
+    file2.open("Data.txt", std::ios::in);
+    file2 >> table3;
+    assert(table3 == table2);
+    table2.deleteKey(3);
+    assert(table2 != table3);
+    output(table3);
+    table.deleteAll();
+    assert(table.getLength() == 0);
+    assert(table != table2);
+    std::cout << "All is OK!" << std::endl;
+    return 0;
+}
